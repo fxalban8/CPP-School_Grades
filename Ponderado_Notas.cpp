8101c12320fcf8aa4be8e93fc6523572d684201e
fxalban08 <francisco.alban@epn.edu.ec>
2020-06-01 21:54:24 -0500

    Writting what the program is about

--- Ponderado_Notas.cpp
+++ Ponderado_Notas.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+/* Exercise 7: A student's final grade is the weighted average of three grades:
+the practical note that counts 30% of the total,
+the theoretical note that counts 60%
+and the participation note that counts the remaining 10%.
+Write a program that reads a student's three notes from standard input
+and write your final grade on standard output.
+*/
+
+
+int main(){
+	
+
+
+}
